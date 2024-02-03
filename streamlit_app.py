1879af2f69137c906adbea5f53f867807d0e397c
Yash Upadhyaya <56195055+yashupadhyaya25@users.noreply.github.com>
2024-02-03 20:04:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,7 @@
 import streamlit as st
 
-st.title('Welcome To My Profile')
+st.title('My Mom's Favourite Healthy Dinner')
 
-st.header('About Me')
-st.text('Data Enigneer & Analyst')
-
-st.header('Tech Stack')
-st.text('Azure Data Factory')
-st.text('Python')
-st.text('Apache Airflow & Airbyte')
-st.text('Azure Logic Apps & Power Automate')
-st.text('Flask')
-st.text('Power BI & AWS Quick Sight')
+st.header('Breakfast Favourite')
+st.text('Hard Boiled Egg')
+st.text('Avocado Toast')
