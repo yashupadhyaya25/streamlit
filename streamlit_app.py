abaa3ea521f0bbd68e51ccf8ce2e6de3de469354
Yash Upadhyaya <56195055+yashupadhyaya25@users.noreply.github.com>
2024-02-03 20:06:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ st.title("My Mom's Favourite Healthy Dinner")
 st.header('Breakfast Favourite')
 st.text('Hard Boiled Egg')
 st.text('Avocado Toast')
+
+st.title("Build Your Own Smoothie")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
