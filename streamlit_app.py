8ea91aca07388fd34a1efdde8e4ecf25fbc37b79
Yash Upadhyaya <56195055+yashupadhyaya25@users.noreply.github.com>
2024-02-03 20:54:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ st.text('Avocado Toast')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 st.title("Build Your Own Smoothie")
-st.text("Filter Fruits By Name")
-fruit_selected = st.multiselect('',list(my_fruit_list.index))
-st.dataframe(my_fruit_list.loc[fruit_selected])
+fruit_selected = st.multiselect('Choose Fruit',list(my_fruit_list.index))
+if fruit_selected.values > 0 :
+  st.dataframe(my_fruit_list.loc[fruit_selected])
+else :
+  st.dataframe(my_fruit_list)
