48d07a248ee91114f30508a06ac56aa981baadf7
Yash Upadhyaya <56195055+yashupadhyaya25@users.noreply.github.com>
2024-02-03 20:42:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,7 @@ st.text('Hard Boiled Egg')
 st.text('Avocado Toast')
 
 st.title("Build Your Own Smoothie")
+st.text("Filter Fruits By Name")
+st.mutiselect(list(my_fruit_list.index))
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
