cf147bb3076cd9d39b7304d3382ea4d7124cf4a0
Yash Upadhyaya <56195055+yashupadhyaya25@users.noreply.github.com>
2024-02-03 20:58:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-st.title("My Mom's Favourite Healthy Dinner")
+st.title("💪🥗 My Mom's Favourite Healthy Dinner 🥗💪")
 
-st.header('Breakfast Favourite')
+st.header('🍳 Breakfast Favourite 🍳')
 st.text('Hard Boiled Egg')
 st.text('Avocado Toast')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-st.title("Build Your Own Smoothie")
-fruit_selected = st.multiselect('Choose Fruit',list(my_fruit_list.index))
+st.title("🥤 Build Your Own Smoothie 🥤")
+fruit_selected = st.multiselect('🍓🍉🍒🍑 Choose Fruit 🍓🍉🍒🍑',list(my_fruit_list.index))
 if len(fruit_selected) > 0 :
   st.dataframe(my_fruit_list.loc[fruit_selected])
 else :
