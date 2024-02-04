145afd731480ad674b31f416a4158f83ff0532da
Yash Upadhyaya <56195055+yashupadhyaya25@users.noreply.github.com>
2024-02-04 20:38:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,5 @@ else :
 
 fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
 st.header("Fruityvice Fruit Advice!")
-st.header(fruityvice_response.jaon())
+st.header(fruityvice_response.json())
 
