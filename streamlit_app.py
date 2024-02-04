a09cc70c629411e309069f74160a72b0cda279cd
Yash Upadhyaya <56195055+yashupadhyaya25@users.noreply.github.com>
2024-02-04 22:56:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,10 +34,8 @@ my_cur.execute("SELECT * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchall()
 fruit_load_list = pd.DataFrame(my_data_row).set_index(0)
 add_fruit_to_load = st.multiselect('🍓🍉🍒🍑 Choose Fruit For Load🍓🍉🍒🍑',list(fruit_load_list.index))
-st.text("Fruit Load List Contains")
-st.dataframe(my_data_row)
 if len(add_fruit_to_load) > 0 :
-  st.write('Thanks For adding',str(add_fruit_to_load)[1:-1])
+  st.write('Thanks For adding',str(add_fruit_to_load)[1:-1].replace("'",""))
 else :
   st.text('Have you forget to add something from fruit load ?')
 
