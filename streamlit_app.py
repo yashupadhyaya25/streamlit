35479fffaa191957e8430ae9822c65a82888fce8
Yash Upadhyaya <56195055+yashupadhyaya25@users.noreply.github.com>
2024-02-04 22:30:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests as rq
-import snowflake.connector as snowflake_connector
+import snowflake.connector
 
 st.title("💪🥗 My Mom's Favourite Healthy Dinner 🥗💪")
 
